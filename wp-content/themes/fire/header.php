98dbe4b1d74a4f72d51b58a3d97e969a0532da34
TR2E <john@skycatchfire.com>
2025-11-05 20:08:52 -0500

    Optimize font loading in header; added preconnect links to Google Fonts for improved performance and removed font import from CSS file to streamline loading process.

--- wp-content/themes/fire/header.php
+++ wp-content/themes/fire/header.php
@@ -26,6 +26,11 @@ if (!function_exists('get_field')) {
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="https://gmpg.org/xfn/11">
+
+  <!-- Preconnect to Google Fonts for better performance -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
   <?php
     $bugherd_api_key = get_field('bugherd_api_key', 'site_settings');
     $bugherd_enabled = get_field('bugherd_enabled', 'site_settings');
